19f74b08d374a49142d9bf9f1d812f2c027a59f4
Meghan Bomberger <meghanbomberger@gmail.com>
2020-01-18 22:40:35 -0700

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>hive</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+
+      </aside>
+      <div class="content">
+
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        users: []
+      }
+    }
+  }
+</script>
