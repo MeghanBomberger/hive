0ff8619ce1a5c705404cca44d0481ebd6e3d357d
Meghan Bomberger <meghanbomberger@gmail.com>
2020-01-20 00:07:35 -0700

    start of user cards

---
+++ src/components/user-card.vue
@@ -0,0 +1,22 @@
+<template>
+	<figure class="user-card" key="clonedUser.id">
+		<p>{{ clonedUser.name }}</p>
+	</figure>
+</template>
+
+<script>
+export default {
+	name: "UserCard",
+	props: {
+		user: {
+			type: Object,
+			default: () => {}
+		}
+	},
+	data () {
+		return {
+			clonedUser: { ...this.user }
+		}
+	}
+}
+</script>
