33fafb4cce6105088af446b5a60b958bdd46a4cf
Meghan Bomberger <meghanbomberger@gmail.com>
2020-01-19 18:14:56 -0700

    initial app set up

---
+++ src/components/header.vue
@@ -0,0 +1,11 @@
+<template>
+	<header>
+		<p>This is a header</p>
+	</header>
+</template>
+
+<script>
+export default {
+	name: 'Header'
+}
+</script>
---
+++ src/components/navbar.vue
@@ -0,0 +1,11 @@
+<template>
+	<nav>
+		<p>This is a navbar</p>
+	</nav>
+</template>
+
+<script>
+export default {
+	name: 'NavBar'
+}
+</script>
